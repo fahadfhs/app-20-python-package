d02b161fe287faf3f18f319fe5b4c8fb646751d5
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-10-09 14:09:47 -0400

    Encapsulated the whole code into a function that was created. function has the path parameters, image parameters, and the different column names.

--- invoice.py
+++ invoice.py
@@ -1,70 +1,75 @@
+import os
+
 import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
 
 
-filepaths = glob.glob("invoices/*.xlsx")
-print(filepaths)
+def generate(invoices_path, pdfs_path, image_path, product_id, product_name,
+             amount_purchased, price_per_unit, total_price):  # adding variables for diff relative paths
+    filepaths = glob.glob(f"{invoices_path}/*.xlsx")  # dynamic path
+    print(filepaths)
 
-for filepath in filepaths:
+    for filepath in filepaths:
 
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
-    pdf.add_page()
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
 
-    filename = Path(filepath).stem
-    # Here we assign the values of the split serially
-    invoice_nr, date = filename.split("-")
+        filename = Path(filepath).stem
+        # Here we assign the values of the split serially
+        invoice_nr, date = filename.split("-")
 
-    pdf.set_font(family="Times", size=16, style="B")
-    # ln = 1 shows to add the next line to a new break line
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+        pdf.set_font(family="Times", size=16, style="B")
+        # ln = 1 shows to add the next line to a new break line
+        pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
-    # AttributeError: 'int' object has no attribute 'replace', this error is because
-    # the values are expected to be string and they are not, hence we convert them
-    # to str, look below
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+        # AttributeError: 'int' object has no attribute 'replace', this error is because
+        # the values are expected to be string and they are not, hence we convert them
+        # to str, look below
 
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    # Add a header, dont need to convert to list we are just iterating, hence
-    # we can do that on index object
-    columns = df.columns
-    columns = [item.replace("_", " ").title() for item in columns]
-    pdf.set_font(family="Times", size=10, style="B")
-    pdf.set_text_color(80, 80, 80)
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
-    pdf.cell(w=30, h=8, txt=columns[3], border=1)
-    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+        df = pd.read_excel(filepath, sheet_name="Sheet 1")
+        # Add a header, dont need to convert to list we are just iterating, hence
+        # we can do that on index object
+        columns = df.columns
+        columns = [item.replace("_", " ").title() for item in columns]
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=columns[0], border=1)
+        pdf.cell(w=70, h=8, txt=columns[1], border=1)
+        pdf.cell(w=30, h=8, txt=columns[2], border=1)
+        pdf.cell(w=30, h=8, txt=columns[3], border=1)
+        pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
-    # Adding rows to the table
-    for index, row in df.iterrows():
+        # Adding rows to the table
+        for index, row in df.iterrows():
+            pdf.set_font(family="Times", size=10)
+            pdf.set_text_color(80, 80, 80)
+            pdf.cell(w=30, h=8, txt=str(row[product_id]), border=1)  # removing hard coding and adding dynamic names
+            pdf.cell(w=70, h=8, txt=str(row[product_name]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[amount_purchased]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[price_per_unit]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[total_price]), border=1, ln=1)
+
+        total_sum = df[total_price].sum()
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
-        pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=70, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt="", border=1)
+        pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
-    total_sum = df["total_price"].sum()
-    pdf.set_font(family="Times", size=10)
-    pdf.set_text_color(80, 80, 80)
-    pdf.cell(w=30, h=8, txt="", border=1)
-    pdf.cell(w=70, h=8, txt="", border=1)
-    pdf.cell(w=30, h=8, txt="", border=1)
-    pdf.cell(w=30, h=8, txt="", border=1)
-    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+        # add total sum sentence
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
 
-    # add total sum sentence
-    pdf.set_font(family="Times", size=10, style="B")
-    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+        # Add company name and logo
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=25, h=8, txt=f"PythonHow")
+        pdf.image(image_path, w=10)
 
-    # Add company name and logo
-    pdf.set_font(family="Times", size=14, style="B")
-    pdf.cell(w=25, h=8, txt=f"PythonHow")
-    pdf.image("pythonhow.png", w=10)
-
-    pdf.output(f"PDFs/{filename}.pdf")
+        os.makedirs(pdfs_path) # to create the pdf path for whatever the user enters
+        pdf.output(f"{pdfs_path}/{filename}.pdf")
